1af6fcffbd1a969506f26dd47538e61804a7a193
Jeremy <jeremy.j.mercado@hotmail.com>
2023-07-25 14:55:35 +0200

    Add funcionality to the different buttons

--- script.js
+++ script.js
@@ -2,46 +2,106 @@ let slider = document.getElementById("myRange");
 let output = document.getElementById("grid-size-text");
 let grid = document.getElementById("grid");
 let div = document.createElement("div");
+let mouseDown = false;
+
+let clearButton = document.getElementById("clearButton");
+let eraserButton = document.getElementById("eraserButton");
+let colorPicker = document.getElementById("colorPicker");
+let colorPickerValue = "rgb(0, 0, 0)";
+let colorMode = document.getElementById("colorMode");
+let rainbowMode = document.getElementById("rainbowMode");
+
+let isRainbowMode = false;
+let isColorMode = false;
+
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false; 
 
 slider.step = "16";
 slider.value = "16";
 slider.max = "64";
 
+clearButton.addEventListener("click", CreateGrid);
+eraserButton.addEventListener("click", Eraser);
+colorPicker.addEventListener("change", ColorPicker);
+colorMode.addEventListener("click", ColorPicker);
+rainbowMode.addEventListener("click", RainbowMode);
+
 output.textContent = `${slider.value} x ${slider.value}`;
 
 
-for (let i = 0; i < slider.value; i++) 
-{
-    for (let j = 0; j < slider.value; j++) 
-    {
-        div = document.createElement("div");
-        div.classList = "element";
-        grid.appendChild(div);   
-    }  
-}
+CreateGrid();
 
 slider.oninput = () =>
 {
     grid = document.getElementById("grid");
     grid.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`;
+        
+    CreateGrid();
 
+    output.textContent = `${slider.value} x ${slider.value}`;
+}
 
+function SetElementColor(e) 
+{
+    if(e.type === "mouseover" && !mouseDown)
+    {
+        return;
+    }
+    else if(isRainbowMode)
+    {
+        colorPickerValue = `rgb(${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)})`;
+
+        e.target.style.backgroundColor = colorPickerValue;
+    }
+    else
+    {
+        e.target.style.backgroundColor = colorPickerValue;
+    }
+}
+
+function CreateGrid() 
+{
     while(grid.firstChild)
     {
         grid.replaceChildren();
     }
-    
-        
+
     for (let i = 0; i < slider.value; i++) 
     {
         for (let j = 0; j < slider.value; j++) 
         {
             div = document.createElement("div");
             div.classList = "element";
+            div.addEventListener("mousedown", SetElementColor)
+            div.addEventListener("mouseover", SetElementColor)
             grid.appendChild(div);   
         }  
     } 
+}
 
-    output.textContent = `${slider.value} x ${slider.value}`;
+function Eraser() 
+{
+    colorPickerValue = "rgb(255,255,255)";
+    isColorMode = false;
+    isRainbowMode = false;
+}
+
+function ColorPicker() 
+{
+    colorPickerValue = document.getElementById("colorPicker").value;
+    isColorMode = true;
+    isRainbowMode = false;
+}
+
+function RainbowMode() 
+{
+    isRainbowMode = true;
+    isColorMode = false;
+}
+
+function getRandomInt(max) 
+{
+    return Math.floor(Math.random() * max);
 }
